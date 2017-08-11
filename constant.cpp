ea6f402e2e53db74f03e97f233c66b44c3ffd69c
Juan Lozano <jclozanos@unal.edu.co>
2017-08-11 08:57:12 -0700

    Programa hecho en clase del 11/08

---
+++ constant.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+const double PI=3.14159;
+const char newline='\n';
+
+int main(void)
+{
+  double r=5.0;
+  double circle;
+  circle=2*PI*r;
+  cout<<circle;
+  cout<<newline;
+  return 0;
+
+}
---
+++ strings.cpp
@@ -0,0 +1,11 @@
+//my first string
+#include <iostream>
+#include <string>
+int main(void)
+{
+  std::string mystring, mystring2;
+  mystring="This is a string";
+  mystring2=" Otra cadena";
+  std::cout<<mystring+mystring2<<std::endl;
+  return 0;
+}
